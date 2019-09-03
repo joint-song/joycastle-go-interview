16da71245133ef52e573c046085c71007cb83f43
joint-song <xuguang1992@vip.qq.com>
2019-09-03 23:01:40 +0800

    Add go module and testing for uniqid

--- pkg/uniqid/pseudo_encrypt_test.go
+++ pkg/uniqid/pseudo_encrypt_test.go
@@ -1,8 +1,28 @@
 package uniqid
 
 import (
+	"fmt"
 	"testing"
+
+	"github.com/smartystreets/goconvey/convey"
 )
 
 func TestPseudoEncrypt(t *testing.T) {
+	convey.Convey("Define input value", t, func() {
+		from, to := 1000, 1010
+		// uniqMap := &sync.Map{}
+		uniqMap := make(map[uint64]struct{}, to-from+1)
+		for i := from; i <= to; i++ {
+			convey.Convey(fmt.Sprint("Generate id by value: ", i), func() {
+				r := pseudoEncrypt(uint64(i))
+				convey.Convey("Id should not present before", func() {
+					convey.So(uniqMap, convey.ShouldNotContainKey, r)
+				})
+				uniqMap[r] = struct{}{}
+				convey.Convey(fmt.Sprintf("It should be reversible between input %d and result %d", i, r), func() {
+					convey.So(i, convey.ShouldEqual, pseudoEncrypt(r))
+				})
+			})
+		}
+	})
 }
--- pkg/uniqid/uniq_id_test.go
+++ pkg/uniqid/uniq_id_test.go
@@ -1,8 +1,33 @@
 package uniqid_test
 
 import (
+	"context"
+	"fmt"
+	"sync"
 	"testing"
+
+	"joycastle.mobi/go-interview/pkg/uniqid"
+
+	"github.com/smartystreets/goconvey/convey"
 )
 
 func TestUniqID(t *testing.T) {
+	var (
+		uniqMap     = &sync.Map{}
+		ctx, cancel = context.WithCancel(context.Background())
+		m           = uniqid.NewManager(ctx)
+	)
+	convey.Convey("Create manager", t, func() {
+		for i := 0; i < 20; i++ {
+			convey.Convey(fmt.Sprintf("Generate id in round %d", i), func() {
+				id := m.NewID()
+				convey.Convey(fmt.Sprintf("Id %d should not present before", id), func() {
+					_, ok := uniqMap.Load(id)
+					convey.So(ok, convey.ShouldBeFalse)
+				})
+				uniqMap.Store(id, struct{}{})
+			})
+		}
+	})
+	cancel()
 }
